efe8dcfde1348eefdf846fdbcf44f030fcb4235d
nextonesfaster <nextonesfaster@gmail.com>
2024-06-24 18:53:59 +0530

    Add new commands

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::fmt::Display;
 use std::hash::Hash;
 use std::str::FromStr;
 
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 use error::{exit, Result};
 use itertools::Itertools;
 use rand::distributions::uniform::SampleUniform;
-use rand::distributions::WeightedIndex;
+use rand::distributions::{Alphanumeric, DistString, Uniform, WeightedIndex};
 use rand::prelude::{Distribution, SliceRandom};
 use rand::Rng;
 
@@ -50,8 +50,9 @@ enum Command {
         count: bool,
         /// Show the result of every flip in order.
         ///
-        /// This is enabled by default up to a threshold, but using the `count` flag disables
-        /// it. Explicitly passing this flag enables it even with the `count` flag.
+        /// This is enabled by default (up to a max threshold), but using the
+        /// `count` flag disables it. Explicitly passing this flag enables it even
+        /// with the `count` flag.
         #[clap(short = 'A', long)]
         all: bool,
     },
@@ -75,8 +76,9 @@ enum Command {
         count: bool,
         /// Show the result of every choice.
         ///
-        /// This is enabled by default, but using the `count` flag disables
-        /// it. Explicitly passing this flag enables it even with the `count` flag.
+        /// This is enabled by default (up to a max threshold), but using the
+        /// `count` flag disables it. Explicitly passing this flag enables it even
+        /// with the `count` flag.
         #[clap(short = 'A', long)]
         all: bool,
         /// Choose items with repetition.
@@ -107,12 +109,82 @@ enum Command {
         /// The upper bound of the range.
         end: Option<Num>,
     },
+    /// Generates a random alphanumeric string.
+    ///
+    /// The default length of the string is 10 and it is lowercase.
+    #[clap(alias = "str")]
+    String {
+        /// The length of the string.
+        #[clap(short, short_alias = 'n', long, default_value_t = 10)]
+        length: usize,
+        /// The case of the string.
+        #[clap(short, long, default_value_t = Case::Lower, value_enum)]
+        case: Case,
+    },
+    /// Rolls a n-sided die.
+    ///
+    /// By default, rolls a 6-sided die.
+    #[clap(alias = "dice")]
+    Die {
+        /// The length of the string.
+        #[clap(default_value_t = 6)]
+        sides: usize,
+        /// The number of times to roll the die.
+        #[clap(short, short_alias = 'n', long, default_value_t = 1)]
+        times: usize,
+        /// Show the number of times each number was rolled.
+        #[clap(short, long)]
+        count: bool,
+        /// Show the result of every roll.
+        ///
+        /// This is enabled by default (up to a max threshold), but using the
+        /// `count` flag disables it. Explicitly passing this flag enables it even
+        /// with the `count` flag.
+        #[clap(short = 'A', long)]
+        all: bool,
+    },
+    /// Assigns items from one list to another randomly.
+    ///
+    /// Both lists must be of equal length.
+    #[clap(alias = "assn")]
+    Assign {
+        /// The items on the left side of the assignment.
+        #[clap(short, long, use_value_delimiter = true)]
+        left: Vec<String>,
+        /// The items on the right side of the assignment.
+        #[clap(short, long, use_value_delimiter = true)]
+        right: Vec<String>,
+    },
 }
 
-#[derive(Debug, Clone)]
+impl Default for Command {
+    fn default() -> Self {
+        Self::Random {
+            inclusive: false,
+            precision: 2,
+            start: Some(Num::FLOAT_0),
+            end: Some(Num::FLOAT_1),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
 enum Num {
     Float(f64),
     Int(i128),
+}
+
+impl Num {
+    const INT_0: Num = Num::Int(0);
+    const FLOAT_0: Num = Num::Float(0.0);
+    const FLOAT_1: Num = Num::Float(1.0);
+
+    fn as_float(&self) -> f64 {
+        match *self {
+            Self::Int(i) => i as f64,
+            Self::Float(f) => f,
+        }
+    }
 }
 
 impl FromStr for Num {
@@ -135,6 +207,13 @@ impl Display for Num {
     }
 }
 
+#[derive(ValueEnum, Debug, Clone, Copy)]
+enum Case {
+    Lower,
+    Upper,
+    Mixed,
+}
+
 fn random_cmd<T: PartialOrd + SampleUniform + Display>(
     lower: T,
     upper: T,
@@ -154,7 +233,7 @@ fn random_cmd<T: PartialOrd + SampleUniform + Display>(
     Ok(())
 }
 
-fn shuffle_cmd(mut items: Vec<String>) {
+fn shuffle_cmd(items: &mut [String]) {
     let mut rng = rand::thread_rng();
     items.shuffle(&mut rng);
     println!("{}", items.iter().join(", "));
@@ -170,7 +249,8 @@ fn choose_with_repetition<S: Display + Eq + Hash>(
     let dist = WeightedIndex::new(weights)?;
     let mut rng = rand::thread_rng();
 
-    print_selections(count, (0..amount).map(|_| &items[dist.sample(&mut rng)]), all, amount);
+    let selections = (0..amount).map(|_| &items[dist.sample(&mut rng)]);
+    print_selections(selections, count, all, amount);
 
     Ok(())
 }
@@ -182,19 +262,20 @@ fn choose_without_repetition(
     count: bool,
     all: bool,
 ) -> Result<()> {
-    let choices = items.into_iter().zip(weights.into_iter()).collect::<Vec<_>>();
+    let choices = items
+        .into_iter()
+        .zip(weights.into_iter())
+        .collect::<Vec<_>>();
 
-    print_selections(
-        count,
-        choices.choose_multiple_weighted(&mut rand::thread_rng(), amount, |i| i.1)?.map(|(i, _)| i),
-        all,
-        amount,
-    );
+    let selections = choices
+        .choose_multiple_weighted(&mut rand::thread_rng(), amount, |i| i.1)?
+        .map(|(i, _)| i);
+    print_selections(selections, count, all, amount);
 
     Ok(())
 }
 
-fn print_selections<'a, I, D>(count: bool, mut selections: I, all: bool, amount: usize)
+fn print_selections<'a, I, D>(mut selections: I, count: bool, all: bool, amount: usize)
 where
     I: Iterator<Item = &'a D>,
     D: 'a + Display + Eq + Hash,
@@ -226,22 +307,64 @@ where
     }
 }
 
+fn string_cmd(characters: usize, case: Case) {
+    let mut s = Alphanumeric.sample_string(&mut rand::thread_rng(), characters);
+
+    match case {
+        Case::Lower => s.make_ascii_lowercase(),
+        Case::Upper => s.make_ascii_uppercase(),
+        Case::Mixed => (),
+    }
+
+    println!("{s}");
+}
+
+fn die_cmd(sides: usize, times: usize, count: bool, all: bool) -> Result<()> {
+    if sides < 1 {
+        return Err("number of sides must be at least 1".into());
+    }
+
+    let mut rng = rand::thread_rng();
+
+    let distr = Uniform::new_inclusive(1, sides);
+    let roll_die = distr.sample_iter(&mut rng);
+
+    let selections = roll_die.take(times).collect::<Vec<_>>();
+    print_selections(selections.iter(), count, all, times);
+
+    Ok(())
+}
+
+fn assign_cmd(left: &[impl Display], right: &mut [impl Display]) -> Result<()> {
+    if left.len() != right.len() {
+        return Err("`left` and `right` lists of unequal length".into());
+    }
+
+    let mut rng = rand::thread_rng();
+    right.shuffle(&mut rng);
+
+    println!(
+        "{}",
+        left.iter()
+            .zip(right.iter())
+            .map(|(l, r)| format!("{l}: {r}"))
+            .join("\n")
+    );
+
+    Ok(())
+}
+
 fn run_cli() -> Result<()> {
     let app = Cli::parse();
 
-    match app.command.unwrap_or(Command::Random {
-        inclusive: false,
-        precision: 2,
-        start: Some(Num::Float(0.0)),
-        end: Some(Num::Float(1.0)),
-    }) {
+    match app.command.unwrap_or_default() {
         Command::Coin {
             amount,
             count,
             all,
             ..
         } => {
-            let all = all || amount < AMOUNT_THRESHOLD;
+            let all = all || amount <= AMOUNT_THRESHOLD;
             let count = count || !all;
 
             choose_with_repetition(vec!["heads", "tails"], vec![1.0, 1.0], amount, count, all)?
@@ -259,7 +382,7 @@ fn run_cli() -> Result<()> {
                 weights = [1.0].repeat(items.len())
             }
 
-            let all = all || amount < AMOUNT_THRESHOLD;
+            let all = all || amount <= AMOUNT_THRESHOLD;
             let count = count || !all;
 
             if repetition || amount > items.len() {
@@ -269,8 +392,8 @@ fn run_cli() -> Result<()> {
             }
         },
         Command::Shuffle {
-            items, ..
-        } => shuffle_cmd(items),
+            mut items, ..
+        } => shuffle_cmd(&mut items),
         Command::Random {
             mut start,
             mut end,
@@ -278,18 +401,43 @@ fn run_cli() -> Result<()> {
             precision,
             ..
         } => {
-            if start.is_some() && end.is_none() {
-                end = start;
-                start = Some(Num::Int(0));
+            if let (Some(s), true) = (start, end.is_none()) {
+                if s.as_float() < 0.0 {
+                    end = Some(Num::INT_0);
+                } else {
+                    end = start;
+                    start = Some(Num::INT_0);
+                }
             }
 
-            match (start.unwrap_or(Num::Float(0.0)), end.unwrap_or(Num::Float(1.0))) {
+            match (start.unwrap_or(Num::FLOAT_0), end.unwrap_or(Num::FLOAT_1)) {
                 (Num::Int(s), Num::Int(e)) => random_cmd(s, e, inclusive, precision),
                 (Num::Int(s), Num::Float(e)) => random_cmd(s as f64, e, inclusive, precision),
                 (Num::Float(s), Num::Int(e)) => random_cmd(s, e as f64, inclusive, precision),
                 (Num::Float(s), Num::Float(e)) => random_cmd(s, e, inclusive, precision),
             }?
         },
+        Command::String {
+            length: characters,
+            case,
+            ..
+        } => string_cmd(characters, case),
+        Command::Die {
+            sides,
+            times,
+            count,
+            all,
+            ..
+        } => {
+            let all = all || times <= AMOUNT_THRESHOLD;
+            let count = count || !all;
+
+            die_cmd(sides, times, count, all)?
+        },
+        Command::Assign {
+            left,
+            mut right,
+        } => assign_cmd(&left, &mut right)?,
     }
 
     Ok(())
